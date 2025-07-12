7acfd5adcf77df17309c5999ba7ef2ad6d17164e
Deniel <denielrhudson@gmail.com>
2025-07-12 15:01:59 -0300

    Refatora Remover ingredientes

--- src/java/Controllers/removerIngrediente.java
+++ src/java/Controllers/removerIngrediente.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controllers;
 
 import DAO.DaoIngrediente;
@@ -21,21 +16,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
 
-/**
- *
- * @author kener_000
- */
 public class removerIngrediente extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private DaoIngrediente daoIngrediente;
+    private ValidadorCookie validadorCookie;
+
+    public removerIngrediente() {
+        this.daoIngrediente = new DaoIngrediente();
+        this.validadorCookie = new ValidadorCookie();
+    }
+
+    public removerIngrediente(DaoIngrediente daoIngrediente, ValidadorCookie validadorCookie) {
+        this.daoIngrediente = daoIngrediente;
+        this.validadorCookie = validadorCookie;
+    }
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
@@ -43,24 +38,21 @@ public class removerIngrediente extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
         String json = "";
-        
-        ////////Validar Cookie
+
         boolean resultado = false;
-        
-        try{
-        Cookie[] cookies = request.getCookies();
-        ValidadorCookie validar = new ValidadorCookie();
-        
-        resultado = validar.validarFuncionario(cookies);
-        }catch(java.lang.NullPointerException e){}
-        //////////////
-        
+
+        try {
+            Cookie[] cookies = request.getCookies();
+            resultado = validadorCookie.validarFuncionario(cookies);
+        } catch (NullPointerException e) {
+        }
+
         if ((br != null) && resultado) {
             json = br.readLine();
-            byte[] bytes = json.getBytes(ISO_8859_1); 
-            String jsonStr = new String(bytes, UTF_8);            
+            byte[] bytes = json.getBytes(ISO_8859_1);
+            String jsonStr = new String(bytes, UTF_8);
             JSONObject dados = new JSONObject(jsonStr);
-            
+
             Ingrediente ingrediente = new Ingrediente();
             ingrediente.setId_ingrediente(dados.getInt("id"));
             ingrediente.setNome(dados.getString("nome"));
@@ -70,59 +62,33 @@ public class removerIngrediente extends HttpServlet {
             ingrediente.setValor_venda(dados.getDouble("ValorVenda"));
             ingrediente.setTipo(dados.getString("tipo"));
             ingrediente.setFg_ativo(1);
-            
-            DaoIngrediente ingredienteDAO = new DaoIngrediente();
-            ingredienteDAO.remover(ingrediente);
-            
+
+            daoIngrediente.remover(ingrediente);
+
             try (PrintWriter out = response.getWriter()) {
-            out.println("Ingrediente Alterado!");
+                out.println("Ingrediente Alterado!");
             }
         } else {
             try (PrintWriter out = response.getWriter()) {
-            out.println("erro");
+                out.println("erro");
+            }
         }
-        }
-        
-        
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+        return "removerIngrediente Servlet";
+    }
 }
